203bdd618f36cc8c478d9c12458332ac186c39c8
JayMc <mciver.jason@gmail.com>
2015-01-16 14:37:35 +1100

    handle incoming IOT data and Firebase visit

--- Server/app.js
+++ Server/app.js
@@ -11,28 +11,55 @@ var visitRef = new Firebase(config.firebaseUrl+'/visit');
 //Past visits
 var visitsRef = new Firebase(config.firebaseUrl+'/visits');
 
+//POJO representation of the visit
+//when the Firebase visitRef gets a value change event it will update this obj
+//So we can monitor data easily
+var visit = {}; 
+
 //Express routes
 //Receives IOT data
 app.post('/', function (req, res) {
 
-    //set current visit with new data (all connected clients will be updated instantly)
-    visitRef.set({
-    	lightOn: req.body.lightOn
-    })
+    //if light is passed in as on,
+    if(req.body.on == 1){
+        //set light status in current visit and light on datetime to now
+        visitRef.update({
+            lightOn: true,
+            lightOnAt: Date.now()
+        })
 
-    //push into past visits
-    visitsRef.push({
-    	lightOn: req.body.lightOn,
-    	date: Date.now()
-    })
+    }   
+    //if light is passed in as off
+    else if(req.body.on == 0){
+        //set light status to off and light off datetime to now
+        visitRef.update({
+            lightOn: false,
+            lightOffAt: Date.now()
+        })
+
+        //push current visit into visits if it is valid (has a lightOnAt and Off Datetime)
+        if(typeof visit.lightOnAt !== 'undefined' && typeof visit.lightOffAt !== 'undefined'){
+            console.log('pushing into visits')
+            visitsRef.push({
+                lightOn: req.body.on,
+                date: Date.now()
+            })
+            
+        }else{
+            console.log('current visit is not valid')
+        }
+
+    }
 
     return res.json(200)
+
 });
 
 
 //Firebase events handlers
 visitRef.on("value", function(snapshot) {
-  console.log(snapshot.val());
+  //console.log(snapshot.val());
+  visit = snapshot.val();
 }, function (errorObject) {
   console.log("The read failed: " + errorObject.code);
 });
